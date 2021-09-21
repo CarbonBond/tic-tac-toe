78e80f9445ed4e8911103f8d4d960c85c70a1252
Carbon <Cheeseburgerbbgun@gmail.com>
2021-09-21 11:08:16 -0600

    Finished the project with an unbeatable AI, Might add checkboxs instead of putting ai in the text Field

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Create a tic-tac-toe game in the browser
+
+Place name one one of the two fields to play the game.
+if Ai is in the field, an Ai will play the game for you.
--- script.js
+++ script.js
@@ -1,83 +1,296 @@
 
 
 
+
+
+
 //Create a gameboardState module
+const gameboardState = (() => {
     //create an 2d array gameboardMap for the gameboard
-        //create three arrays for each row of the game board
+    let gameboardMap = [
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ]
+
+    resetMap = () => {
+       gameboardMap = [
+        ["","",""],
+        ["","",""],
+        ["","",""]
+        ];
+        displayGame.whipeMap()
+    }
     
-    //create a setTargetMark function with an x, y cordinate and a playermark
+    //create a setLocationMark function with an x, y cordinate and a playermark
+    setLocationMark = (x, y, playermark) => {
         //assign the playermark to the array
+        gameboardMap[x][y] = playermark;
+    }
 
-    //create a getTargetMark with an x, y
+    //create a getLocationMark with an x, y
+    getLocationMark = (x, y) => {
         //return a string at location x,y
+        return gameboardMap[x][y];
+    }
 
+    getBoard = () => {return gameboardMap}
     //return an object that has the edit function within
+    return {
+        setLocationMark,
+        getLocationMark,
+        getBoard,
+        resetMap
+    }
+})();
+
 
 //create a player function factory with playermark and name
-    //create a array varible playerMark
+const playerFunctionFactory = (pName, pMark) =>{
+    //create a varible playerMark
+    let mark = pMark;
+    //create a name varibles
+    let name = pName;
     //create a function that gets playermark
+    getPlayerMark = () => { return mark}
 
     //return a object that conatins the getters and setters
+    return {name, getPlayerMark}
+}
 
 //create a module for controlling the display
+const displayGame = (() => {
+    const tileList = document.querySelectorAll('.tile');
+    const currentPlayerDiv = document.querySelector('.currentPlayer');
+    let clickLocation = null;
 
-    //create a function createGameboard taking gameMaps 2d grid
-        //create a varible gameContainer as a new DOM div
-        //create a varible boardContainer as a new dom div
-        //create a varible controlContainer as a new dom div
-        //create a varible headerContainer as a new dom div
-        //append the last three varibles to gameContainer
+    tileList.forEach(item => {
+        item.addEventListener('click', ()=>{
+            clickLocation = item.classList[1].split('');
+            clickLocation.splice(0,1);
+            gameController.getMove();
+        })
+    })
 
-        //create a varible gameTitle as a new DOM div
-        // assign "Tic tac toe" to textArea
-        // create a varible currentPlayer as a new DOM divs
-        // assign player.getName to textArea
-        //append both gameTitle and currentPlayer to headerContainer
+    whipeMap = () => {
+        tileList.forEach(tile => {
+            tile.textContent = "";
+        })
+    }
+    
+    updateMap = () => {
+        tileList.forEach(tile => {
+            tileLocation = tile.classList[1].split('')
+            tileLocation.splice(0,1);
+            board = gameboardState.getBoard();
+            if(tile.textContent !== board[tileLocation[0]][tileLocation[1]]){
+                tile.textContent = board[tileLocation[0]][tileLocation[1]]
+            }
+        })
+    }
 
-        //create a varible PlayerContainer as a new div
-        //create and append two playerName divs
-        //create a varible playButton as a new button
-        //create a eventlistor on playButton 
-            // call startgame function on game Module
+    updatePlayer = (name) => {
+        currentPlayerDiv.textContent = name;
+    }
+    updateWinner = (win, player) => {
+        if(win == 'tie'){
+        currentPlayerDiv.textContent = `the game was a ${win}`;
+        }else {
+            currentPlayerDiv.textContent = `${player.name} is the winner!`;
+        }
+    }
 
-        //loop over Map array both x and y
-            //create a varible to create a element div
-            //assign varibles classlist to gridItem
-            //set id to array values
-            //append item to gameboardContainer DIV
-            //add eventlisnor 
-                // call playerMove function with x, y as location
-        //return true;
-
+    hideControl = () => {
+        document.querySelector('.playerNameContainer').classList.toggle('hide');
+        document.querySelector('.boardContainer').classList.toggle('hide');
+        document.querySelector('.playButton').classList.toggle('hide');
+        document.querySelector('.restartButton').classList.toggle('hide');
+    }
+    
+    getClickLocation = () => { return clickLocation};
     //create a varible that takes a nodelist of divs items for the grid
     //create a function updateGameDisplay, updating visuals taking player.mark
         //create two loops going over the 2d arrays
             //create a varible for DOM query (ID 11, 12, 12, 21, 22, 23, etc)
             //change varible.inntertext to player mark
 
+    return {getClickLocation, updateMap, whipeMap, updatePlayer, updateWinner, hideControl}
+})();
 
 //create a game module for controling gamestates
+const gameController = (() => { 
     //create currentPlayer value
-    //create playerOne with player factor function
-    //Do this again with playerTwo
+    playButton = document.querySelector('.playButton')
+    playButton.addEventListener('click', ()=>{
+        startGame();
+        })
+    restartButton = document.querySelector('.restartButton')
+    restartButton.addEventListener('click', ()=>{
+        restartGame();
+    })
+    function setPlayers(){//create playerOne with player factor function
+        playerOneInput = document.querySelector(".playerOneName")
+        playerOne = playerFunctionFactory(playerOneInput.value, "X")
+        //Do this again with playerTwo
+        playerTwoInput = document.querySelector(".playerTwoName")
+        playerTwo = playerFunctionFactory(playerTwoInput.value, "O")
+        if(playerOne.name == playerTwo.name){
+            playerTwo.name += "2";
+        }
+    }
+    let playerOne;
+    let playerTwo;
+    let currentPlayer;
 
-    //check if a map is made
-        //if a map isn't made call display.createGameboard
+    //assign currentPlayer to playerOne
 
-    //create a startgame function
-        //set playerOne name based on value of playerOne input
-        //Same but with player two
-        //create two players with names
-        //assign currentPlayer to playerOne
+    let gameStart = false;
 
-        
+    
+    function startGame() {
+        setPlayers();
+        displayGame.hideControl();
+        gameStart=true;
+        currentPlayer = playerOne;
+        displayGame.updatePlayer(currentPlayer.name);
+    }
+    function restartGame() {
+        setPlayers();
+        displayGame.hideControl();
+        gameboardState.resetMap();
+        gameStart=false
+        displayGame.updatePlayer("");
+    }
 
-    //create a varible that determens if the game is started
-    //create a playerMove function the uses selection and player.mark
-        //check if mark as been made at this location alreay
-            //call a setter for gameboard module to update the map
-            //call a display function to update the DOM
-            //check if game is won or tied
+    function checkWin(board){
+        board
+        for(let y = 0; y < board.length; y++){
+            if(!(board[y][0] == "" || board[y][1] == "" || board[y][2] == "" )){
+                if((board[y][0] === board[y][1]) && (board[y][2] === board[y][1])){
+                    return board[y][0];
+                }
+            }
+        }
+        for(let x = 0; x < board.length; x++){
+            if(!(board[0][x] == "" || board[1][x] == "" || board[2][x] == "")){
+                if((board[0][x] === board[1][x]) && (board[1][x] === board[2][x])){
+                    return board[0][x];
+                }
+            }  
+        }
+        if(!(board[0][0] == "" || board[1][1] == "" || board[2][2] == "")){
+            if((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])){
+                return board[1][1];
+            }
+        }        
+        if(!(board[0][2] == "" || board[1][1] == "" || board[2][0] == "")){
+            if((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])){
+                return board[1][1];
+            }
+        }
+        let tie = 0;
+        for(let x = 0; x < board.length; x++){
+            for(let y = 0; y < board.length; y++){
+                if(!(board[y][0] == "" || board[y][1] == "" || board[y][2] == "" )){
+                    if(board[x][y] !== ""){
+                        tie += 1
+                    }
+                }
+            }
+        }        
+        if(tie === 9){
+            return "tie"
+        }
+        return false;
+    }    
+    //create a getMove function the uses selection and player.mark
+    getMove = () => {
+        if(!gameStart){
+            console.log("Game not started");
+            return;
+        }
+        clickLocation = displayGame.getClickLocation();
+        if((currentPlayer.name).toLowerCase() == 'ai' || (currentPlayer.name).toLowerCase() == 'ai2'){
+            let aiMove = getAiMove()
+            if(aiMove){
+                gameboardState.setLocationMark(aiMove[0], aiMove[1], currentPlayer.getPlayerMark())
+            }
+        } else{
+            //check if mark as been made at this location alreay
+            if(gameboardState.getLocationMark(clickLocation[0], clickLocation[1]) === ""){
+                //call a setter for gameboard module to update the map
+                gameboardState.setLocationMark(clickLocation[0], clickLocation[1], currentPlayer.getPlayerMark())
+            } 
+        }
+        //call a display function to update the DOM
+        displayGame.updateMap();
 
+        //check if game is won or tied
+        win = checkWin(gameboardState.getBoard());
+        if(win){
+            displayGame.updateWinner(win, currentPlayer)
+            gameStart = false;
+        } else {
+            if(playerOne === currentPlayer){ 
+                currentPlayer = playerTwo;
+            } 
+            else {currentPlayer = playerOne}  
+            displayGame.updatePlayer(currentPlayer.name);
+        }       
+    }
+    function getAiMove(){
+        board = gameboardState.getBoard();
+        let boardCopy = [Array.from(board[0]), Array.from(board[1]), Array.from(board[2])]
+        //Checking for winning move
+        for(let x = 0; x < boardCopy.length; x++){
+            for(let y = 0; y < boardCopy[x].length; y++){
+                boardCopy = [Array.from(board[0]), Array.from(board[1]), Array.from(board[2])]
+                if(boardCopy[x][y] === ""){
+                    boardCopy[x][y] = currentPlayer.getPlayerMark()
+                    if(checkWin(boardCopy)){
+                        return[x, y]
+                    }
+                }
+            }
+        }
+        //Checking for player win
+        for(let x = 0; x < boardCopy.length; x++){
+            for(let y = 0; y < boardCopy[x].length; y++){
+                boardCopy = [Array.from(board[0]), Array.from(board[1]), Array.from(board[2])]
+                if(boardCopy[x][y] === ""){
+                    if(currentPlayer.getPlayerMark() === "X"){
+                        boardCopy[x][y] = "O"
+                    }else {
+                        boardCopy[x][y] = "X"
+                    }
+                    if(checkWin(boardCopy)){
+                        return[x, y]
+                    }
+                }
+            }
+        }
+        //Checking for random corner
+        for(let i = 0; i< 4; i++){
+            let corners = [[0,0], [0,2], [2,0], [2,2]]
+            corner = corners[i]
+            if (boardCopy[corner[0]][corner[1]] == ""){
+                return corners[i];
+            }
+        }
+        //check center
+        if(boardCopy[1][1] === ""){
+            return [1, 1]
+        }
+        //check sides
+        for(let i = 0; i< 4; i++){
+            let corners = [[0,1], [1,2], [2,1], [1,0]]
+            corner = corners[i]
+            if (boardCopy[corner[0]][corner[1]] == ""){
+                return corners[i];
+            }
+        }
+    }
 
-            
+    return {getMove, gameStart, startGame}
+})();
+
